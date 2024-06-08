6c660fda8032e7a2230f4ec8ef7641b58df02960
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-06-08 11:17:42 +0500

    slice & splice arrays method in typescript

---
+++ index.ts
@@ -0,0 +1,35 @@
+// Arrays slice Method 
+
+// let Fruits: string[] = ["Mango","Orange","Grapes","Cheery","Banana"]
+// console.log(Fruits); // print original array
+
+// console.log(Fruits.slice(0 , 3))
+
+// console.log(Fruits);
+
+
+// Arrays splice Method 
+
+let programming: string[] = ["JS","TS","Next.js","Python","HTML"]
+console.log(programming);
+
+// console.log(programming.splice(1,1,"React")); // TS
+
+programming.splice(1 , 1 , "React")
+
+console.log(programming);
+
+programming.splice(1 , 0 , "CSS", "TS")
+
+console.log(programming);
+
+
+
+
+
+
+
+
+
+
+
